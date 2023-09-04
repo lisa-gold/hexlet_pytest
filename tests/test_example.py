941f98e07723a4eeacec832ee7c4c8db440f923c
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2023-09-04 18:37:31 +0300

    add tests to test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,3 +5,31 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+
+def test_stack():
+    stack = []
+    # Добавляем два элемента в стек и затем извлекаем их
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)  # not not stack
+
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    # проверить что вызывается конкретное исключение можно с помощью конструкции with pytest.raises()
+    # если внутри блока вызовется исключение, то тест будет пройден
+    with pytest.raises(IndexError):
+        stack.pop()
